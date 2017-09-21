76b01e9d715c85e295f3f7eec9e302221cc77f42
UMI-NOTE\UMI <f91seed@yahoo.co.jp>
2017-09-21 22:02:26 +0900

    first maven project

---
+++ src/main/java/pink/nico/bootdevstudy/BootDevstudyApplication.java
@@ -0,0 +1,12 @@
+package pink.nico.bootdevstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDevstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootDevstudyApplication.class, args);
+	}
+}
